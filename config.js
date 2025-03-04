92f8a02c884347662809da15bef1ad5826e3b1a7
smkov <53491160+smkov@users.noreply.github.com>
2025-03-04 19:24:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// config.js
 const CONFIG = {
-    SITE_KEY: "0x4AAAAAAA_W6xNKpQ-7finl"  // Site Key из Cloudflare
+    SITE_KEY: "0x4AAAAAAA_W6xNKpQ-7finl",  // Site Key из Cloudflare
+    END_POINT: "https://nameless-star-cc22.smkov.workers.dev/" // URL бэкенда
 };
 
 // Экспортируем объект CONFIG
